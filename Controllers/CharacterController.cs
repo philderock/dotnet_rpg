1c3cd2cceb157ce15d976d9d65488a3cebc28a36
Philip <cchukwuma.p@gmail.com>
2023-05-07 20:49:43 +0100

    Dependency Injection implemented

---
+++ Controllers/CharacterController.cs
@@ -0,0 +1,40 @@
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using dotnet_rpg.Services.CharacterService;
+using Microsoft.AspNetCore.Mvc;
+
+namespace dotnet_rpg.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class CharacterController : ControllerBase
+    {
+        
+        private readonly ICharacterService _characterService ;
+
+        public CharacterController(ICharacterService characterService)
+        {
+            _characterService = characterService;
+        }
+        [HttpGet("GetAll")]
+        public ActionResult<List<Character>> Get(){
+            return Ok(_characterService.GetAllCharacters());
+        }
+
+        
+
+        [HttpGet("{id}")]
+        public ActionResult<List<Character>> GetById(int id){
+            return Ok(_characterService.GetCharacterById(id));
+        }
+        [HttpPost]
+        public ActionResult<List<Character>> AddCharacter(Character newCharacter)
+        {
+            
+            return Ok(_characterService.AddCharacter(newCharacter));
+        }
+    }
+}
