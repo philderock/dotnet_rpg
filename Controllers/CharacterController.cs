3fa2602f94896bc02cd57d3e66fbff06ebf065e8
Philip <cchukwuma.p@gmail.com>
2023-05-11 18:04:00 +0100

    ServiceResponse Added

--- Controllers/CharacterController.cs
+++ Controllers/CharacterController.cs
@@ -20,21 +20,21 @@ namespace dotnet_rpg.Controllers
             _characterService = characterService;
         }
         [HttpGet("GetAll")]
-        public ActionResult<List<Character>> Get(){
-            return Ok(_characterService.GetAllCharacters());
+        public async Task<ActionResult<ServiceResponse<List<Character>>>> Get(){
+            return Ok(await _characterService.GetAllCharacters());
         }
 
         
 
         [HttpGet("{id}")]
-        public ActionResult<List<Character>> GetById(int id){
-            return Ok(_characterService.GetCharacterById(id));
+        public async Task<ActionResult<ServiceResponse<List<Character>>>> GetById(int id){
+            return Ok(await _characterService.GetCharacterById(id));
         }
         [HttpPost]
-        public ActionResult<List<Character>> AddCharacter(Character newCharacter)
+        public async Task<ActionResult<ServiceResponse<List<Character>>>> AddCharacter(Character newCharacter)
         {
             
-            return Ok(_characterService.AddCharacter(newCharacter));
+            return Ok(await _characterService.AddCharacter(newCharacter));
         }
     }
 }
--- Services/CharacterService/CharacterService.cs
+++ Services/CharacterService/CharacterService.cs
@@ -14,25 +14,28 @@ namespace dotnet_rpg.Services.CharacterService
         };
 
         
-        public List<Character> AddCharacter(Character newCharacter)
+        public async Task<ServiceResponse<List<Character>>> AddCharacter(Character newCharacter)
         {
+            var serviceResponse = new ServiceResponse<List<Character>>();
             characters.Add(newCharacter);
-            return characters;
+            serviceResponse.Data = characters;
+            return serviceResponse;
         }
 
-        public List<Character> GetAllCharacters()
+        public async Task<ServiceResponse<List<Character>>> GetAllCharacters()
         {
-            return characters;
+            var serviceResponse = new ServiceResponse<List<Character>>();
+            serviceResponse.Data = characters;
+            return serviceResponse;
         }
 
-        // public Character GetCharacterById(int id)
-        // {
-        //     return characters.FirstOrDefault( c => c.Id == id);
-        // }
 
-        public Character GetCharacterById(int id)
+        public async Task<ServiceResponse<Character>> GetCharacterById(int id)
         {
-            return characters.FirstOrDefault(c => c.Id == id);
+            var serviceResponse =new ServiceResponse<Character>();
+            var character =  characters.FirstOrDefault(c => c.Id == id);
+            serviceResponse.Data = character;
+            return serviceResponse;
         }
     }
 }
